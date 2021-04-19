77997bba53a038afd869a621892fb4c655d36a12
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-19 18:22:29 +0200

    format code

--- src/main/java/com/capdevon/control/TimerControl.java
+++ src/main/java/com/capdevon/control/TimerControl.java
@@ -4,27 +4,31 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.control.AbstractControl;
 
+/**
+ * 
+ * @author capdevon
+ */
 public abstract class TimerControl extends AbstractControl {
 
-	float lifeTime = 0;
-	float maxLifeTime = 0;
+    float lifeTime = 0;
+    float maxLifeTime = 0;
 
-	public TimerControl(float maxLifeTime) {
-		this.maxLifeTime = maxLifeTime;
-	}
+    public TimerControl(float maxLifeTime) {
+        this.maxLifeTime = maxLifeTime;
+    }
 
-	@Override
-	protected void controlUpdate(float tpf) {
-		lifeTime += tpf;
-		if (lifeTime > maxLifeTime) {
-			onTrigger();
-		}
-	}
+    @Override
+    protected void controlUpdate(float tpf) {
+        lifeTime += tpf;
+        if (lifeTime > maxLifeTime) {
+            onTrigger();
+        }
+    }
 
-	@Override
-	protected void controlRender(RenderManager rm, ViewPort vp) {
-		// TODO Auto-generated method stub
-	}
-	
-	public abstract void onTrigger();
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        // TODO Auto-generated method stub
+    }
+
+    public abstract void onTrigger();
 }
