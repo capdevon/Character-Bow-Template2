8b722765462dbcc3383f9decfa9c71a129a29399
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-19 18:04:02 +0200

    format code

--- src/main/java/mygame/CrosshairData.java
+++ src/main/java/mygame/CrosshairData.java
@@ -15,43 +15,43 @@ import com.jme3.scene.Node;
  */
 public class CrosshairData {
 
-	// The BitmapText that will be used for this weapon's crosshair
-	public BitmapText bitmapText;
-	// parent node
-	public Node guiNode;
+    // The BitmapText that will be used for this weapon's crosshair
+    public BitmapText bitmapText;
+    // parent node
+    public Node guiNode;
 
-	/**
-	 * 
-	 * @param guiNode
-	 * @param bmp
-	 */
-	public CrosshairData(Node guiNode, BitmapText bmp) {
-		this.guiNode = guiNode;
-		this.bitmapText = bmp;
-	}
+    /**
+     * 
+     * @param guiNode
+     * @param bmp
+     */
+    public CrosshairData(Node guiNode, BitmapText bmp) {
+        this.guiNode = guiNode;
+        this.bitmapText = bmp;
+    }
 
-	public void setEnabled(boolean enabled) {
-		if (enabled) {
-			guiNode.attachChild(bitmapText);
-		} else {
-			guiNode.detachChild(bitmapText);
-		}
-	}
+    public void setEnabled(boolean enabled) {
+        if (enabled) {
+            guiNode.attachChild(bitmapText);
+        } else {
+            guiNode.detachChild(bitmapText);
+        }
+    }
 
-	public float getSize() {
-		return bitmapText.getSize();
-	}
+    public float getSize() {
+        return bitmapText.getSize();
+    }
 
-	public void setSize(float size) {
-		bitmapText.setSize(size);
-	}
+    public void setSize(float size) {
+        bitmapText.setSize(size);
+    }
 
-	public ColorRGBA getColor() {
-		return bitmapText.getColor();
-	}
+    public ColorRGBA getColor() {
+        return bitmapText.getColor();
+    }
 
-	public void setColor(ColorRGBA color) {
-		bitmapText.setColor(color);
-	}
+    public void setColor(ColorRGBA color) {
+        bitmapText.setColor(color);
+    }
 
 }
