1f495f9d9095e87ebcbbb68362ed08866a9cfcdb
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-19 18:05:35 +0200

    format code

--- src/main/java/mygame/Weapon.java
+++ src/main/java/mygame/Weapon.java
@@ -14,50 +14,50 @@ import java.util.ArrayList;
  */
 public class Weapon {
 
-	private String name;
-	private Node weaponHook;
-	private Node model;
+    private String name;
+    private Node weaponHook;
+    private Node model;
 
-	CrosshairData crosshair;
-	float distance = 30f;
-	float damage = 50f;
-	int ammo = 20;
+    CrosshairData crosshair;
+    float distance = 30f;
+    float damage = 50f;
+    int ammo = 20;
 
-	private int ammoTypeIndex = 0;
-	private final ArrayList<String> ammoTypes;
+    private int ammoTypeIndex = 0;
+    private final ArrayList<String> ammoTypes;
 
-	/**
-	 * Create new Weapon
-	 * 
-	 * @param name
-	 * @param weaponHook
-	 * @param model
-	 */
-	public Weapon(String name, Node weaponHook, Node model) {
-		this.weaponHook = weaponHook;
-		this.model = model;
-		this.name = name;
-		this.ammoTypes = new ArrayList<>();
-	}
+    /**
+     * Create new Weapon
+     * 
+     * @param name
+     * @param weaponHook
+     * @param model
+     */
+    public Weapon(String name, Node weaponHook, Node model) {
+        this.weaponHook = weaponHook;
+        this.model = model;
+        this.name = name;
+        this.ammoTypes = new ArrayList<>();
+    }
 
-	public void setActive(boolean active) {
-		if (active) {
-			weaponHook.attachChild(model);
-		} else {
-			weaponHook.detachChild(model);
-		}
-	}
+    public void setActive(boolean active) {
+        if (active) {
+            weaponHook.attachChild(model);
+        } else {
+            weaponHook.detachChild(model);
+        }
+    }
 
-	public void onChangeAmmo() {
-		ammoTypeIndex = (ammoTypeIndex + 1) % ammoTypes.size();
-		System.out.println("AmmoType: " + ammoTypeIndex);
-	}
+    public void onChangeAmmo() {
+        ammoTypeIndex = (ammoTypeIndex + 1) % ammoTypes.size();
+        System.out.println("AmmoType: " + ammoTypeIndex);
+    }
 
-	public String getAmmoType() {
-		return ammoTypes.get(ammoTypeIndex);
-	}
+    public String getAmmoType() {
+        return ammoTypes.get(ammoTypeIndex);
+    }
 
-	public void addAmmoType(String model) {
-		ammoTypes.add(model);
-	}
+    public void addAmmoType(String model) {
+        ammoTypes.add(model);
+    }
 }
