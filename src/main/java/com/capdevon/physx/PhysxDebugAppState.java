a32559d610076680457c241bb12bc10b289fa9ee
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-19 18:16:47 +0200

    format code

--- src/main/java/com/capdevon/physx/PhysxDebugAppState.java
+++ src/main/java/com/capdevon/physx/PhysxDebugAppState.java
@@ -13,40 +13,39 @@ import com.jme3.input.controls.KeyTrigger;
  * @author capdevon
  */
 public class PhysxDebugAppState extends BaseAppState implements ActionListener {
-	
-	private static final String TOGGLE_PHYSICS_DEBUG = "TOGGLE_PHYSICS_DEBUG";
-	
-	private BulletAppState bulletAppState;
-	private InputManager inputManager;
-	
-	@Override
-	protected void initialize(Application app) {
-		this.bulletAppState = getState(BulletAppState.class, true);
-		this.inputManager = app.getInputManager();
-	}
-	
-	@Override
-	protected void cleanup(Application app) {
-	}
 
-	@Override
-	protected void onEnable() {
-		inputManager.addMapping(TOGGLE_PHYSICS_DEBUG, new KeyTrigger(KeyInput.KEY_0));
-		inputManager.addListener(this, TOGGLE_PHYSICS_DEBUG);
-	}
+    private static final String TOGGLE_PHYSICS_DEBUG = "TOGGLE_PHYSICS_DEBUG";
 
-	@Override
-	protected void onDisable() {
-		inputManager.deleteMapping(TOGGLE_PHYSICS_DEBUG);
-		inputManager.removeListener(this);
-	}
+    private BulletAppState bulletAppState;
+    private InputManager inputManager;
 
-	@Override
-	public void onAction(String name, boolean isPressed, float tpf) {
-		if (name.equals(TOGGLE_PHYSICS_DEBUG) && isPressed) {
+    @Override
+    protected void initialize(Application app) {
+        this.bulletAppState = getState(BulletAppState.class, true);
+        this.inputManager = app.getInputManager();
+    }
+
+    @Override
+    protected void cleanup(Application app) {}
+
+    @Override
+    protected void onEnable() {
+        inputManager.addMapping(TOGGLE_PHYSICS_DEBUG, new KeyTrigger(KeyInput.KEY_0));
+        inputManager.addListener(this, TOGGLE_PHYSICS_DEBUG);
+    }
+
+    @Override
+    protected void onDisable() {
+        inputManager.deleteMapping(TOGGLE_PHYSICS_DEBUG);
+        inputManager.removeListener(this);
+    }
+
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        if (name.equals(TOGGLE_PHYSICS_DEBUG) && isPressed) {
             boolean debug = bulletAppState.isDebugEnabled();
             bulletAppState.setDebugEnabled(!debug);
         }
-	}
+    }
 
 }
