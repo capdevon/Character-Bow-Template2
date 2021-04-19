f316ac80ec42c8bbbef8a39576122fe7ef649ec3
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-19 18:01:58 +0200

    format code

--- src/main/java/mygame/ParticleManager.java
+++ src/main/java/mygame/ParticleManager.java
@@ -21,86 +21,84 @@ import com.jme3.scene.Spatial;
  */
 public class ParticleManager extends BaseAppState {
 
-	private SimpleApplication simpleApp;
-	private Node rootLocal;
+    private SimpleApplication simpleApp;
+    private Node rootLocal;
 
-	@Override
-	protected void initialize(Application app) {
-		this.simpleApp = (SimpleApplication) app;
-		rootLocal = new Node("Particles");
-	}
+    @Override
+    protected void initialize(Application app) {
+        this.simpleApp = (SimpleApplication) app;
+        rootLocal = new Node("Particles");
+    }
 
-	@Override
-	protected void cleanup(Application app) {
-		rootLocal.detachAllChildren();
-	}
+    @Override
+    protected void cleanup(Application app) {
+        rootLocal.detachAllChildren();
+    }
 
-	@Override
-	protected void onEnable() {
-		// TODO Auto-generated method stub
-		simpleApp.getRootNode().attachChild(rootLocal);
-	}
+    @Override
+    protected void onEnable() {
+        simpleApp.getRootNode().attachChild(rootLocal);
+    }
 
-	@Override
-	protected void onDisable() {
-		// TODO Auto-generated method stub
-		simpleApp.getRootNode().attachChild(rootLocal);
-	}
+    @Override
+    protected void onDisable() {
+        simpleApp.getRootNode().attachChild(rootLocal);
+    }
 
-	public void playEffect(String name, Vector3f location, float lifeTime) {
+    public void playEffect(String name, Vector3f location, float lifeTime) {
 
-		Node emitter = (Node) simpleApp.getAssetManager().loadModel(name);
-		emitter.setLocalTranslation(location);
-		rootLocal.attachChild(emitter);
+        Node emitter = (Node) simpleApp.getAssetManager().loadModel(name);
+        emitter.setLocalTranslation(location);
+        rootLocal.attachChild(emitter);
 
-		EmitterData data = new EmitterData(emitter);
-		emitter.addControl(new TimerControl(lifeTime) {
-			@Override
-			public void onTrigger() {
-				data.stop();
-				rootLocal.detachChild(emitter);
-			}
-		});
+        EmitterData data = new EmitterData(emitter);
+        emitter.addControl(new TimerControl(lifeTime) {
+            @Override
+            public void onTrigger() {
+                data.stop();
+                rootLocal.detachChild(emitter);
+            }
+        });
 
-		// play effect
-		data.play();
-	}
+        // play effect
+        data.play();
+    }
 
-	/**
-	 * ------------------------------------------------------------------
-	 * @EmitterData 
-	 * ------------------------------------------------------------------
-	 */
-	private class EmitterData {
+    /**
+     * ------------------------------------------------------------------
+     * @EmitterData 
+     * ------------------------------------------------------------------
+     */
+    private class EmitterData {
 
-		public Node emitter;
+        public Node emitter;
 
-		public EmitterData(Node emitter) {
-			this.emitter = emitter;
-		}
+        public EmitterData(Node emitter) {
+            this.emitter = emitter;
+        }
 
-		protected void stop() {
-			for (Spatial sp : emitter.getChildren()) {
-				if (sp instanceof AudioNode) {
-					((AudioNode) sp).stop();
+        protected void stop() {
+            for (Spatial sp: emitter.getChildren()) {
+                if (sp instanceof AudioNode) {
+                    ((AudioNode) sp).stop();
 
-				} else if (sp instanceof ParticleEmitter) {
-					((ParticleEmitter) sp).killAllParticles();
-				}
-			}
-		}
+                } else if (sp instanceof ParticleEmitter) {
+                    ((ParticleEmitter) sp).killAllParticles();
+                }
+            }
+        }
 
-		protected void play() {
-			for (Spatial sp : emitter.getChildren()) {
-				if (sp instanceof AudioNode) {
-					((AudioNode) sp).play();
+        protected void play() {
+            for (Spatial sp: emitter.getChildren()) {
+                if (sp instanceof AudioNode) {
+                    ((AudioNode) sp).play();
 
-				} else if (sp instanceof ParticleEmitter) {
-					((ParticleEmitter) sp).emitAllParticles();
-				}
-			}
-		}
+                } else if (sp instanceof ParticleEmitter) {
+                    ((ParticleEmitter) sp).emitAllParticles();
+                }
+            }
+        }
 
-	}
+    }
 
 }
