d398c59bfddb15cd41eae4b5e5cc4ec40d7e8d72
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-19 17:54:49 +0200

    Update CrosshairData.java

--- src/main/java/mygame/CrosshairData.java
+++ src/main/java/mygame/CrosshairData.java
@@ -8,66 +8,50 @@ package mygame;
 import com.jme3.font.BitmapText;
 import com.jme3.math.ColorRGBA;
 import com.jme3.scene.Node;
-import com.jme3.ui.Picture;
 
 /**
- *
+ * 
+ * @author capdevon
  */
 public class CrosshairData {
-    
-    // The BitmapText that will be used for this weapon's crosshair
-    public BitmapText bitmapText;
-    // The image that will be used for this weapon's crosshair
-    public Picture picture;
-    // The size of the crosshair image
-    public int size;
-    // The color of the crosshair image
-    public ColorRGBA color;
-    // parent node
-    public Node guiNode;
-    
-    public CrosshairData(Node guiNode, Picture picture) {
-        this.guiNode = guiNode;
-        this.picture = picture;
-        this.size = 1;
-        this.color = ColorRGBA.White.clone();
-    }
-    
-    public CrosshairData(Node guiNode, BitmapText bmp) {
-        this.guiNode = guiNode;
-        this.bitmapText = bmp;
-        this.size = 1;
-        this.color = ColorRGBA.White.clone();
-    }
-    
-    public void setEnabled(boolean enabled) {
-        int i = -1;
-        
-        if (picture != null)
-            i = enabled ? guiNode.attachChild(picture) : guiNode.detachChild(picture);
-        else if (bitmapText != null)
-            i = enabled ? guiNode.attachChild(bitmapText) : guiNode.detachChild(bitmapText);
-//        System.out.println("showCrosshair: " + enabled + " gui.numberOfChildren: " + i);
-    }
 
-    public int getSize() {
-        return size;
-    }
+	// The BitmapText that will be used for this weapon's crosshair
+	public BitmapText bitmapText;
+	// parent node
+	public Node guiNode;
 
-    public void setSize(int size) {
-        this.size = size;
-    }
+	/**
+	 * 
+	 * @param guiNode
+	 * @param bmp
+	 */
+	public CrosshairData(Node guiNode, BitmapText bmp) {
+		this.guiNode = guiNode;
+		this.bitmapText = bmp;
+	}
 
-    public ColorRGBA getColor() {
-        return color;
-    }
-
-	public void setColor(ColorRGBA color) {
-		this.color.set(color);
-		if (bitmapText != null) {
-			bitmapText.setColor(color);
+	public void setEnabled(boolean enabled) {
+		if (enabled) {
+			guiNode.attachChild(bitmapText);
+		} else {
+			guiNode.detachChild(bitmapText);
 		}
 	}
-    
-}
 
+	public float getSize() {
+		return bitmapText.getSize();
+	}
+
+	public void setSize(float size) {
+		bitmapText.setSize(size);
+	}
+
+	public ColorRGBA getColor() {
+		return bitmapText.getColor();
+	}
+
+	public void setColor(ColorRGBA color) {
+		bitmapText.setColor(color);
+	}
+
+}
