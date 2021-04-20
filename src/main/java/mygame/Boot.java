6728d23d5ea98a6d47ff0bf360a7dad795ba4098
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 18:06:42 +0200

    Add files via upload

---
+++ src/main/java/mygame/Boot.java
@@ -0,0 +1,36 @@
+package mygame;
+
+import java.util.Arrays;
+
+import com.capdevon.engine.Scene;
+import com.capdevon.input.GInputAppState;
+import com.capdevon.physx.PhysxDebugAppState;
+import com.jme3.app.DetailedProfilerState;
+import com.jme3.bullet.BulletAppState;
+
+import mygame.states.CubeAppState;
+import mygame.states.SceneAppState;
+
+/**
+ * 
+ * @author capdevon
+ */
+public class Boot {
+	
+    public static final Scene scene_01 = new Scene();
+    static {
+        scene_01.name = "Scene 1";
+        scene_01.systemPrefabs = Arrays.asList(
+        		BulletAppState.class,
+        		PhysxDebugAppState.class,
+                SceneAppState.class,
+                CubeAppState.class,
+                GInputAppState.class,
+                ParticleManager.class,
+                PlayerManager.class,
+                DetailedProfilerState.class
+                //BasicProfilerState
+        );
+    }
+
+}
