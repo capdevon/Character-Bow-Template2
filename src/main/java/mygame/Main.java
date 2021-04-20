cc993393ceda9fd399caf88b8755e6cf20b5ff3d
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 18:09:39 +0200

    add Boot

--- src/main/java/mygame/Main.java
+++ src/main/java/mygame/Main.java
@@ -9,6 +9,7 @@ import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 
+import com.capdevon.engine.SceneManager;
 import com.capdevon.input.GInputAppState;
 import com.capdevon.physx.PhysxDebugAppState;
 import com.jme3.app.FlyCamAppState;
@@ -25,50 +26,61 @@ import mygame.states.SceneAppState;
  */
 public class Main extends SimpleApplication {
 
-    /**
-     * Start the jMonkeyEngine application
-     * @param args
-     */
-    public static void main(String[] args) {
-        //Get the Resolution of the main/default display
-        GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        DisplayMode display = device.getDisplayMode();
+	/**
+	 * Start the jMonkeyEngine application
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Get the Resolution of the main/default display
+		GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		DisplayMode display = device.getDisplayMode();
 
-        Main app = new Main();
+		Main app = new Main();
 
-        AppSettings settings = new AppSettings(true);
-        settings.setResolution(1280, 720);
-        settings.setFrameRate(60);
-        settings.setBitsPerPixel(display.getBitDepth());
-        settings.setFrequency(display.getRefreshRate());
-        settings.setSamples(4);
-        settings.setVSync(true);
-        settings.setGammaCorrection(true);
-        // settings.setUseJoysticks(true);
+		AppSettings settings = new AppSettings(true);
+		settings.setResolution(1280, 720);
+		settings.setFrameRate(60);
+		settings.setBitsPerPixel(display.getBitDepth());
+		settings.setFrequency(display.getRefreshRate());
+		settings.setSamples(4);
+		settings.setVSync(true);
+		settings.setGammaCorrection(true);
+		settings.setUseJoysticks(true);
 
-        app.setSettings(settings);
-        app.setShowSettings(true);
-        app.setPauseOnLostFocus(false);
-        app.start();
-    }
+		app.setSettings(settings);
+		app.setShowSettings(true);
+		app.setPauseOnLostFocus(false);
+		app.start();
+	}
 
-    @Override
-    public void simpleInitApp() {
-        // disable the default 1st-person flyCam!
-        stateManager.detach(stateManager.getState(FlyCamAppState.class));
-        flyCam.setEnabled(false);
+	@Override
+	public void simpleInitApp() {
+		// disable the default 1st-person flyCam!
+		stateManager.detach(stateManager.getState(FlyCamAppState.class));
+		flyCam.setEnabled(false);
 
-        /** Initialize the physics simulation */
-        BulletAppState physics = new BulletAppState();
-        stateManager.attach(physics);
-        physics.setDebugEnabled(false);
+		stateManager.attach(new SceneManager());
 
-        stateManager.attach(new SceneAppState());
-        stateManager.attach(new CubeAppState());
-        stateManager.attach(new PhysxDebugAppState());
-        stateManager.attach(new GInputAppState());
-        stateManager.attach(new ParticleManager());
-        stateManager.attach(new PlayerManager());
-    }
+		/** Initialize the physics simulation */
+//        stateManager.attach(new BulletAppState());
+//        stateManager.attach(new PhysxDebugAppState());
+//        stateManager.attach(new SceneAppState());
+//        stateManager.attach(new CubeAppState());
+//        stateManager.attach(new GInputAppState());
+//        stateManager.attach(new ParticleManager());
+//        stateManager.attach(new PlayerManager());
+	}
+
+	boolean sceneLoaded = false;
+
+	@Override
+	public void simpleUpdate(float tpf) {
+		// To change body of generated methods, choose Tools | Templates.
+		if (!sceneLoaded) {
+			stateManager.getState(SceneManager.class).loadScene(Boot.scene_01);
+			sceneLoaded = true;
+		}
+	}
 
 }
