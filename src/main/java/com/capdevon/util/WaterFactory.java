65e2d41df1e6e3d3a0834b00da9ed26d7ee45d98
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 10:27:35 +0200

    format code

--- src/main/java/com/capdevon/util/WaterFactory.java
+++ src/main/java/com/capdevon/util/WaterFactory.java
@@ -9,31 +9,35 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.water.SimpleWaterProcessor;
 
+/**
+ * 
+ * @author capdevon
+ */
 public class WaterFactory {
 
-	private AssetManager assetManager;
-	private ViewPort viewPort;
+    private AssetManager assetManager;
+    private ViewPort viewPort;
 
-	public WaterFactory(AssetManager assetManager, ViewPort viewPort) {
-		this.assetManager = assetManager;
-		this.viewPort = viewPort;
-	}
+    public WaterFactory(AssetManager assetManager, ViewPort viewPort) {
+        this.assetManager = assetManager;
+        this.viewPort = viewPort;
+    }
 
-	public Geometry build(Node scene, float width, float height) {
-		SimpleWaterProcessor wpss = new SimpleWaterProcessor(assetManager);
-		viewPort.addProcessor(wpss);
+    public Geometry build(Node scene, float width, float height) {
+        SimpleWaterProcessor wpss = new SimpleWaterProcessor(assetManager);
+        viewPort.addProcessor(wpss);
 
-		wpss.setPlane(new Plane(Vector3f.UNIT_Y, 0));
-		wpss.setReflectionScene(scene);
-		wpss.setReflectionClippingOffset(-0.1f);
-		wpss.setDistortionScale(0.2f);
-		wpss.setWaterDepth(0.4f);
-		wpss.setWaveSpeed(0.06f);
+        wpss.setPlane(new Plane(Vector3f.UNIT_Y, 0));
+        wpss.setReflectionScene(scene);
+        wpss.setReflectionClippingOffset(-0.1f);
+        wpss.setDistortionScale(0.2f);
+        wpss.setWaterDepth(0.4f);
+        wpss.setWaveSpeed(0.06f);
 
-		Geometry waterGeo = wpss.createWaterGeometry(width, height);
-		waterGeo.setMaterial(wpss.getMaterial());
-		waterGeo.setShadowMode(ShadowMode.Off);
+        Geometry waterGeo = wpss.createWaterGeometry(width, height);
+        waterGeo.setMaterial(wpss.getMaterial());
+        waterGeo.setShadowMode(ShadowMode.Off);
 
-		return waterGeo;
-	}
+        return waterGeo;
+    }
 }
