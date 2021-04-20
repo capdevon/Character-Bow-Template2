58b5993dec2259de3dc9e7b37bd42c90833bf699
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-04-20 16:35:06 +0200

    format code

--- src/main/java/mygame/AmmoType.java
+++ src/main/java/mygame/AmmoType.java
@@ -5,10 +5,10 @@ package mygame;
  * @author capdevon
  */
 public class AmmoType {
-	
-	String name;
-	String effect;
-	float explosionRadius = 5;
+
+    String name;
+    String effect;
+    float explosionRadius = 5;
     float baseStrength = 10f;
 
 }
