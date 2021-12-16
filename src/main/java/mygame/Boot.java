d30851249029977a25a0e906f6df4af758d97b35
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-12-16 10:26:50 +0100

    format code

--- src/main/java/mygame/Boot.java
+++ src/main/java/mygame/Boot.java
@@ -12,24 +12,25 @@ import mygame.states.CubeAppState;
 import mygame.states.SceneAppState;
 
 /**
- * 
+ *
  * @author capdevon
  */
 public class Boot {
-	
+
     public static final Scene scene_01 = new Scene();
+
     static {
         scene_01.name = "Scene 1";
         scene_01.systemPrefabs = Arrays.asList(
-        		BulletAppState.class,
-        		PhysxDebugAppState.class,
+                BulletAppState.class,
+                PhysxDebugAppState.class,
                 SceneAppState.class,
                 CubeAppState.class,
                 GInputAppState.class,
                 ParticleManager.class,
                 PlayerManager.class,
                 DetailedProfilerState.class
-                //BasicProfilerState
+                //BasicProfilerState.class
         );
     }
 
