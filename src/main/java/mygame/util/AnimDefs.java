74f1004debb08c3f2da76c7987cd73d46721f789
capdevon <jefferydeaver2010@gmail.com>
2025-03-03 13:01:04 +0100

    AnimDefs: code review

--- src/main/java/mygame/util/AnimDefs.java
+++ src/main/java/mygame/util/AnimDefs.java
@@ -8,16 +8,16 @@ import com.capdevon.anim.Animation3;
  */
 public interface AnimDefs {
 
-    final String ARCHER_ASSET_MODEL = "Models/Archer/YBot.j3o";
+    String ARCHER_ASSET_MODEL = "Models/Archer/YBot.j3o";
 
-    final Animation3 Idle                   = new Animation3("Idle", true);
-    final Animation3 Idle2                  = new Animation3("Idle_2", true);
-    final Animation3 Running                = new Animation3("Running", true);
-    final Animation3 Sprinting              = new Animation3("Sprinting", true);
-    final Animation3 StandingIdle           = new Animation3("StandingIdle", true, .2f);
-    final Animation3 StandingAimIdle        = new Animation3("StandingAimIdle", false);
-    final Animation3 StandingAimOverdraw    = new Animation3("StandingAimOverdraw", false);
-    final Animation3 StandingAimRecoil      = new Animation3("StandingAimRecoil", false);
-    final Animation3 StandingDrawArrow      = new Animation3("StandingDrawArrow", false);
+    Animation3 Idle                   = new Animation3("Idle", true);
+    Animation3 Idle2                  = new Animation3("Idle_2", true);
+    Animation3 Running                = new Animation3("Running", true);
+    Animation3 Sprinting              = new Animation3("Sprinting", true);
+    Animation3 StandingIdle           = new Animation3("StandingIdle", true, .2f);
+    Animation3 StandingAimIdle        = new Animation3("StandingAimIdle", false);
+    Animation3 StandingAimOverdraw    = new Animation3("StandingAimOverdraw", false);
+    Animation3 StandingAimRecoil      = new Animation3("StandingAimRecoil", false);
+    Animation3 StandingDrawArrow      = new Animation3("StandingDrawArrow", false);
 
 }
