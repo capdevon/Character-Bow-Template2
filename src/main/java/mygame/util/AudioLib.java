19a3fc5bd29d41bb5c4984d60c2c557c3b0ed20b
capdevon <jefferydeaver2010@gmail.com>
2025-03-03 13:01:21 +0100

    AudioLib: code review

--- src/main/java/mygame/util/AudioLib.java
+++ src/main/java/mygame/util/AudioLib.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mygame.util;
 
 import com.capdevon.audio.AudioClip;
 
-/**
- *
- */
 public interface AudioLib {
 
     AudioClip ENV_NATURE        = new AudioClip("Sound/Environment/Nature.ogg", 4f, true, false);
