a449463ea0471c11a7fbf24acc145ef896a4e1bf
capdevon <jefferydeaver2010@gmail.com>
2025-03-03 13:08:29 +0100

    KeyMapping: code review

--- src/main/java/com/capdevon/input/KeyMapping.java
+++ src/main/java/com/capdevon/input/KeyMapping.java
@@ -6,28 +6,28 @@ package com.capdevon.input;
  */
 public interface KeyMapping {
 
-    final String MOVE_FORWARD       = "MOVE_FORWARD";
-    final String MOVE_BACKWARD      = "MOVE_BACKWARD";
-    final String MOVE_LEFT          = "MOVE_LEFT";
-    final String MOVE_RIGHT         = "MOVE_RIGHT";
-    final String TOGGLE_HANGING     = "TOGGLE_HANGING";
-    final String TOGGLE_CROUCH      = "TOGGLE_CROUCH";
-    final String TOGGLE_COVER       = "TOGGLE_COVER";
-    final String TRIGGER_ACTION     = "TRIGGER_ACTION";
-    final String RUNNING            = "RUNNING";
-    final String AIMING             = "AIMING";
-    final String FIRE               = "FIRE";
-    final String JUMP               = "JUMP";
-    final String AUTO_AIM           = "AUTO_AIM";
-    final String RELOAD_WEAPON      = "RELOAD_WEAPON";
-    final String SWITCH_WEAPON      = "SWITCH_WEAPON";
-    final String RESET_POSITION     = "RESET_POSITION";
-    final String TOGGLE_FLASHLIGHT  = "TOGGLE_FLASHLIGHT";
-    final String TOGGLE_FLYCAMERA   = "TOGGLE_FLYCAMERA";
+    String MOVE_FORWARD       = "MOVE_FORWARD";
+    String MOVE_BACKWARD      = "MOVE_BACKWARD";
+    String MOVE_LEFT          = "MOVE_LEFT";
+    String MOVE_RIGHT         = "MOVE_RIGHT";
+    String TOGGLE_HANGING     = "TOGGLE_HANGING";
+    String TOGGLE_CROUCH      = "TOGGLE_CROUCH";
+    String TOGGLE_COVER       = "TOGGLE_COVER";
+    String TRIGGER_ACTION     = "TRIGGER_ACTION";
+    String RUNNING            = "RUNNING";
+    String AIMING             = "AIMING";
+    String FIRE               = "FIRE";
+    String JUMP               = "JUMP";
+    String AUTO_AIM           = "AUTO_AIM";
+    String RELOAD_WEAPON      = "RELOAD_WEAPON";
+    String SWITCH_WEAPON      = "SWITCH_WEAPON";
+    String RESET_POSITION     = "RESET_POSITION";
+    String TOGGLE_FLASHLIGHT  = "TOGGLE_FLASHLIGHT";
+    String TOGGLE_FLYCAMERA   = "TOGGLE_FLYCAMERA";
 
-    final String KEY_1 = "KEY_1";
-    final String KEY_2 = "KEY_2";
-    final String KEY_3 = "KEY_3";
-    final String KEY_4 = "KEY_4";
-    final String KEY_5 = "KEY_5";
+    String KEY_1 = "KEY_1";
+    String KEY_2 = "KEY_2";
+    String KEY_3 = "KEY_3";
+    String KEY_4 = "KEY_4";
+    String KEY_5 = "KEY_5";
 }
