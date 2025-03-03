30277321798b519474854966cffa4ec45a82be6a
capdevon <jefferydeaver2010@gmail.com>
2025-03-03 13:03:56 +0100

    add Damageable class

---
+++ src/main/java/com/capdevon/control/Damageable.java
@@ -0,0 +1,34 @@
+package com.capdevon.control;
+
+import com.jme3.material.MatParamOverride;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.control.AbstractControl;
+import com.jme3.shader.VarType;
+
+/**
+ * @author capdevon
+ */
+public class Damageable extends AbstractControl {
+    
+    private MatParamOverride mpo;
+
+    @Override
+    protected void controlUpdate(float tpf) {
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+    }
+    
+    public void takeDamage(ColorRGBA color) {
+        if (mpo == null) {
+            mpo = new MatParamOverride(VarType.Vector4, "Color", color);
+            spatial.addMatParamOverride(mpo);
+        } else {
+            mpo.setValue(color);
+        }
+    }
+
+}
