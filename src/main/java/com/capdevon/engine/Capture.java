39fe111ccba8556e3c140f2f2052fd57688c79e4
capdevon <jefferydeaver2010@gmail.com>
2025-03-04 20:34:03 +0100

    Capture: code review

--- src/main/java/com/capdevon/engine/Capture.java
+++ src/main/java/com/capdevon/engine/Capture.java
@@ -1,15 +1,21 @@
 package com.capdevon.engine;
 
+import java.io.File;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.jme3.app.Application;
 import com.jme3.app.state.VideoRecorderAppState;
 import com.jme3.system.AppSettings;
-import java.io.File;
 
 /**
  *
  * @author capdevon
  */
 public class Capture {
+    
+    private static final Logger logger = Logger.getLogger(Capture.class.getName());
 
     /**
      * A private constructor to inhibit instantiation of this class.
@@ -18,25 +24,26 @@ public class Capture {
     }
 
     /**
-     * @param app
-     * @param quality [0.5f]
-     * @param dirName
+     * Captures a video recording of the application's output.
+     *
+     * @param app     The Application instance for which to capture video.
+     * @param quality The quality of the video recording, ranging from 0.0f to 1.0f (default: 0.5f).
      */
-    public static void captureVideo(Application app, float quality, String dirName) {
+    public static void captureVideo(Application app, float quality) {
         AppSettings settings = app.getContext().getSettings();
-        long fileId = (System.currentTimeMillis() / 1000);
-        String fileName = settings.getTitle() + "-" + fileId + ".avi";
-        File file = new File(dirName, fileName);
-
         int frameRate = settings.getFrameRate();
         if (settings.getFrameRate() < 0) {
             throw new IllegalArgumentException("FrameRate must not be negative: " + frameRate);
         }
-
+        
+        String dirName = System.getProperty("user.dir");
+        String fileName = UUID.randomUUID() + ".avi";
+        File file = new File(dirName, fileName);
+        
         VideoRecorderAppState recorder = new VideoRecorderAppState(file, quality, frameRate);
         app.getStateManager().attach(recorder);
 
-        System.out.println("Start VideoRecorder=" + file.getAbsolutePath());
+        logger.log(Level.WARNING, "Start VideoRecorder=" + file.getAbsolutePath());
     }
 
 }
