e3284bcc6d955912ab2935b757660833a90bf88b
capdevon <jefferydeaver2010@gmail.com>
2025-07-12 16:38:04 +0200

    Damageable: update

--- src/main/java/com/capdevon/control/Damageable.java
+++ src/main/java/com/capdevon/control/Damageable.java
@@ -4,6 +4,7 @@ import com.jme3.material.MatParamOverride;
 import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.control.AbstractControl;
 import com.jme3.shader.VarType;
 
@@ -11,8 +12,18 @@ import com.jme3.shader.VarType;
  * @author capdevon
  */
 public class Damageable extends AbstractControl {
-    
+
     private MatParamOverride mpo;
+
+    @Override
+    public void setSpatial(Spatial sp) {
+        super.setSpatial(sp);
+        if (spatial != null) {
+            mpo = new MatParamOverride(VarType.Vector4, "BaseColor", ColorRGBA.White);
+            spatial.addMatParamOverride(mpo);
+            mpo.setEnabled(false);
+        }
+    }
 
     @Override
     protected void controlUpdate(float tpf) {
@@ -21,14 +32,10 @@ public class Damageable extends AbstractControl {
     @Override
     protected void controlRender(RenderManager rm, ViewPort vp) {
     }
-    
+
     public void takeDamage(ColorRGBA color) {
-        if (mpo == null) {
-            mpo = new MatParamOverride(VarType.Vector4, "BaseColor", color);
-            spatial.addMatParamOverride(mpo);
-        } else {
-            mpo.setValue(color);
-        }
+        mpo.setValue(color);
+        mpo.setEnabled(true);
     }
 
 }
